f273e601f07462f361863373d366febfc8dc468d
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2023-02-03 18:26:11 -0300

    Working

--- peer.py
+++ peer.py
@@ -1,97 +1,76 @@
 import socket
 from threading import Thread
-import os
+from random import randint
 
-host = socket.gethostbyname(socket.gethostname())
-port = 2000
-port2 = 2001
-id = None
+peer_ip = socket.gethostbyname(socket.gethostname())
+peer_port = randint(2001, 3000)
 
-peer_list = []
+tracker_port = 2000
+
+id = 0
 contact_list = {}
 
-print(f"HOST: {host} PORT: {port}")
+print(f"IP: {peer_ip} PORT: {peer_port}")
 
-tracker = input("Digite o endereço ip do super nó: ")
-connect_to = tracker
+tracker_ip = input("Digite o endereço ip do super nó: ")
+connect_to = (tracker_ip, tracker_port)
 
-# cliente
-peer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPV4 | SOCK_STREAM = TCP
-peer.connect((tracker, port)) # conecta ao servidor
-peer.sendto(f"ID,{host}, aa".encode("utf-8"), (tracker, port)) # envia a mensagem codificada em bits
+svr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+svr.bind((peer_ip, peer_port))
+svr.listen(5)
 
+clt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clt.connect((tracker_ip, tracker_port))
+clt.send(f"ID;{peer_ip};{peer_port}".encode("utf-8"))
 
-# servidor
-peer2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPV4 | SOCK_STREAM = TCP
-peer2.bind((host, port2)) # bind = vincular
-peer2.listen(4) # limites de conexões
+def check():
+    return True
 
-def conec():
-    global peer2
-
-    con, adr = peer2.accept()
-
-def check(contact):
-    global contact_list
-
-    if contact in contact_list:
-        return contact_list[contact]
-    else:
-        return False
-
-def recive():
+def server():
     global id
+    global connect_to
+    global clt
 
-    
-    
+
     while True:
-        data = peer.recv(1024) # 1024 = tamanho do buffer
-        decoded_data = data.decode("utf-8")
-        print(decoded_data) 
+        con, adr = svr.accept()
 
-        data1, data2, data3 = decoded_data.split(",")
-        print("debug")
-        
-        if data1 == "ID":
-            id = data3
-            print(f"meu id é : {id}")
-            print(f"minha porta é: {port2}")
+        while True:
+            data = con.recv(1024)
+            commands = data.decode("utf-8")
 
-        elif data1 != id:
-            peer.sendto(data, connect_to) # envia a mensagem codificada em bits
+            for command in commands.split("X"):
+                if command == "":
+                    continue
 
-        elif data1 == id:
-            if data2 == "CONNECT_WITH":
-                peer.close()
-                peer.connect((data3, port))
-                connect_to = data3
+                print(f"Comando recebido por p{id}: {command}") 
 
-            if data2 == "NEW_ID":
-                id = data3
-                data3 = str(int(data3) + 1)
-                peer.sendto(f"{data1},{data2},{data3}", connect_to)
-            if data2 == "CHECK_CONTACT":
-                result = check(data3)
+                data1, data2, data3 = command.split(";")
 
-                if result == False:
-                    peer.sendto(data, connect_to)
-                
-                print("oi")
+                if (data1 == "ID") and (id == 0):
+                    id = data3
+                    print(f"MEU NOVO ID: {id}")
 
-def send():
-    while True:
-        comando = input("\nDigite a mensagem: \n")
+                elif data1 != f"P{id}":
+                    clt.sendto(f"{command}X".encode("utf-8"), connect_to) # envia a mensagem codificada em bits
 
-        if comando == "exit":
-            peer.sendto(f"TK,REMOVE_FROM_LIST,{id}".encode("utf-8"), (connect_to, port))
-            peer.close()
-            break
-        else:
-            msg = f"TK,DEBUG,aaaaaaadebugg{id}"
-            peer.sendto(comando.encode("utf-8"), (connect_to, port))
+                elif data1 == f"P{id}":
+                    if data2 == "CONNECT_WITH":
+                        clt.close()
+                        
+                        connect_to = (data3[2:16], int(data3[19:23]))
+                        print(connect_to)
+                        clt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        clt.connect(connect_to)
 
-Thread(target=send).start()
-Thread(target=recive).start()
-Thread(target=conec).start()
-print("test")
+                    if data2 == "NEW_ID":
+                        id = data3
+                        data3 = int(data3) + 1
+                        clt.sendto(f"{data1};{data2};{data3}X".encode("utf-8"), connect_to)
+                    if data2 == "CHECK_CONTACT":
+                        result = check(data3)
 
+                        if result == False:
+                            clt.sendto(data, connect_to)
+
+Thread(target=server).start()
--- tracker.py
+++ tracker.py
@@ -1,76 +1,93 @@
 import socket
 from threading import Thread
 
-localHost = socket.gethostbyname(socket.gethostname())
-localPort = 2000
+# arrumer porta aleatoria
+
+connect_to = None
+
+host = socket.gethostbyname(socket.gethostname())
+port = 2000
 
 peers_list = [(socket.gethostbyname(socket.gethostname()), 2000)]
 
-print(f"HOST: {localHost} PORT: {localPort}")
+print(f"HOST: {host} PORT: {port}")
 
-tracker = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-tracker.bind((localHost, localPort))
-tracker.listen(3) # primeiro conectado, ultimo conectado, novo conectado
+# Socket cliente
+clt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def connection():
+# Socket servidor
+svr = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+svr.bind((host, port))
+svr.listen(5)
+
+def server():
+    global peers_list
+    global connect_to
+
     while True:
-        con, adr = tracker.accept()
-        print(adr)
+        con, adr = svr.accept()
 
         while True:
             print(peers_list)
+
             #recebe a mensagem do ultimo par da rede ou de novo par
             data = con.recv(1024)
+            commands = data.decode("utf-8")
+            
+            for command in commands.split("X"):
+                if command == "":
+                    continue
 
-            if not data:
-                break
+                print(f"Comando recebido pelo traker: {command}")
 
-            decoded_data = data.decode("utf-8")
-            print(decoded_data)
+                # data1 = ID | data2 = comando | data3 = informação personalizada
+                data1, data2, data3, = command.split(";")
 
-            # data1 = ID | data2 = comando | data3 = informação personalizada
-            data1, data2, data3, = decoded_data.split(",")
+                # novo par entra na rede
+                if(data1 == "ID"):
+                    peers_list.append((adr[0], int(data3)))
 
-            # novo par entra na rede
-            if(data1 == "ID"):
-                peers_list.append(adr)
-                con.sendto(f"ID, NEW_ID, {len(peers_list) - 1}".encode("utf-8"), peers_list[1])
+                    if(len(peers_list) == 2):
+                        connect_to = peers_list[1]
+                        clt.connect(connect_to)
+                        clt.send(f"ID;NEW_ID;{len(peers_list) - 1}X".encode("utf-8"))
+                    
+                    # se não for o unico par da rede, vai ser o novo ultimo par
+                    if(len(peers_list) > 2):
+                        old_final_peer_number = len(peers_list) - 2
+
+                        # envia a mensagem para a rede
+                        # antigo ultimo par, conecte-se com o novo ultimo par
+                        clt.send(f"P{old_final_peer_number};CONNECT_WITH;{peers_list[-1]}X".encode("utf-8"))
+
+                        # novo ultimo par, esse é seu id
+                        clt.send(f"ID;NEW_ID;{len(peers_list) - 1}X".encode("utf-8"))
+                        
+                        break
+          
+                elif(data1[0] == "P"):
+                    clt.send(command.encode("utf-8"))
                 
-                # se não for o unico par da rede, vai ser o novo ultimo par
-                if(len(peers_list) > 2):
-                    final_peer_number = len(peers_list) - 1
-                    old_final_peer_number = len(peers_list) - 2
+                elif(data1 == "TK"):
+                    # TK, REMOVE_FROM_LIST, NUMERO DO PEER QUE SAIU
+                    if(data2 == "REMOVE_FROM_LIST"):
+                        if data3 == str(len(peers_list) - 1):
+                            del peers_list[-1]
+                            clt.send(f"P{len(peers_list) - 1};CONNECT_WITH;{peers_list[0]}X".encode("utf-8"))
+                        elif data3 != "1":
+                            #numero do peer que saiu
+                            quit_number = int(data3)
+                            del peers_list[quit_number]
+                            before_number = quit_number - 1
+                            after_ip = peers_list[quit_number + 1]
 
-                    # envia a mensagem para a rede
-                    # antigo ultimo par, conecte-se com o novo ultimo par
-                    con.sendto(f"P{old_final_peer_number},CONNECT_WITH,{peers_list[-1]}".encode("utf-8"), peers_list[1])
-                    # novo ultimo par, esse é seu id
-                    con.sendto(f"P{final_peer_number}, NEW_ID, {len(peers_list) - 1}".encode("utf-8"), peers_list[1])
+                            clt.send(f"P{before_number};CONNECT_WITH;{after_ip}X".encode("utf-8"))
+                            clt.send(f"P{quit_number + 1};NEW_ID;{quit_number}X".encode("utf-8"))
+                        if data3 == "1":
+                            del peers_list[1]
+                            clt.send(f"P2;NEW_ID;{1}X", peers_list[1])
+                    elif(data2 == "DEBUG"):
+                        print(f"{data3}")
 
-            
-            elif(data1[0] == "P"):
-                con.sendto(data, peers_list[1])
-            
-            elif(data1 == "TK"):
-                # TK, REMOVE_FROM_LIST, NUMERO DO PEER QUE SAIU
-                if(data2 == "REMOVE_FROM_LIST"):
-                    if data3 == str(len(peers_list) - 1):
-                        del peers_list[-1]
-                        con.sendto(f"P{len(peers_list) - 1},CONNECT_WITH,{peers_list[0]}", peers_list[1])
-                    elif data3 != "1":
-                        #numero do peer que saiu
-                        quit_number = int(data3)
-                        del peers_list[quit_number]
-                        before_number = quit_number - 1
-                        after_ip = peers_list[quit_number + 1]
-
-                        con.sendto(f"P{before_number},CONNECT_WITH, {after_ip}", peers_list[0])
-                        con.sendto(f"P{quit_number + 1},NEW_ID,{quit_number}", peers_list[0])
-                    if data3 == "1":
-                        del peers_list[1]
-                        con.sendto(f"P2,NEW_ID,{1}", peers_list[1])
-                elif(data2 == "DEBUG"):
-                    print(f"{data3}")
-
-for i in range(3):
-    Thread(target=connection).start()
+for i in range(4):
+    Thread(target=server).start()
